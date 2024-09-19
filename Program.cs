84c8f580c1e67f5bc5d6bd770140ed3b99204848
illeiva2 <illeiva@outlook.com>
2024-09-19 10:26:49 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Trabajo_integrador_DSOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
